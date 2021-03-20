4d048e5b9552fd31fb41cdcf431da941839bd650
Svetoslav Stoyanov <426911@student.fontys.nl>
2021-03-20 15:06:59 +0100

    Update MainActivity.java

--- TestApp/app/src/main/java/com/vogella/android/testapp/MainActivity.java
+++ TestApp/app/src/main/java/com/vogella/android/testapp/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //assign layout to an activity
         setContentView(R.layout.activity_main);
     }
 }
