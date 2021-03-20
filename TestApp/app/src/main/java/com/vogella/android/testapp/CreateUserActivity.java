7a84742f09c06755825095a20e00d5e59e345450
Svetoslav Stoyanov <426911@student.fontys.nl>
2021-03-20 18:11:00 +0100

    up

--- TestApp/app/src/main/java/com/vogella/android/testapp/CreateUserActivity.java
+++ TestApp/app/src/main/java/com/vogella/android/testapp/CreateUserActivity.java
@@ -3,13 +3,17 @@ package com.vogella.android.testapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class CreateUserActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //assign layout to an activity
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_create_user);
+    }
+
+    public void onClick(View view) {
     }
 }
