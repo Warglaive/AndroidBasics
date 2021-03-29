3093a99dc553bfbdba3360486fddc4f34207f753
Svetoslav Stoyanov <426911@student.fontys.nl>
2021-03-29 11:25:14 +0200

    up

--- TestApp/app/src/main/java/com/vogella/android/testapp/CreateUserActivity.java
+++ TestApp/app/src/main/java/com/vogella/android/testapp/CreateUserActivity.java
@@ -5,33 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Switch;
 import android.widget.Toast;
 
 public class CreateUserActivity extends AppCompatActivity {
     boolean male = true;
-    boolean female = true;
-    RadioGroup radioGroup = (RadioGroup) findViewById(R.id.gender);
-    //TODO: FIX
-    radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-        @Override
-        public void onCheckedChanged(RadioGroup group, int checkedId) {
-            switch (checkedId) {
-                case R.id.male:
-                    male = true;
-                    break;
-                case R.id.female:
-                    male = false;
-                    break;
-            }
-        }
-    });
-
-
-
-
-
-
+    //boolean female = true;
 
 
     @Override
@@ -39,11 +20,29 @@ public class CreateUserActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         //assign layout to an activity
         setContentView(R.layout.activity_create_user);
+
     }
 
     public void onClick(View view) {
+        RadioGroup radioGroup = findViewById(R.id.gender);
+
         EditText text = findViewById(R.id.username);
         String toastMsg = text.getText().toString();
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton btnF = findViewById(R.id.female);
+                RadioButton btnM = findViewById(R.id.male);
+                if (checkedId == R.id.male) {
+                    male = true;
+                    btnF.setAlpha(0);
+                } else if (checkedId == R.id.female) {
+                    male = false;
+                    btnM.setAlpha(0);
+                }
+            }
+        });
         Toast.makeText(this, "User: " + toastMsg + " created.", Toast.LENGTH_LONG).show();
+
     }
 }
